c67435076aa5e3840f070b5305e0639a14f9528b
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-02-15 12:23:47 +0100

    Strings opdracht - eind versie
    Ingeleverd op woensdag 15-02-2023 ergens tussen 12:23 en 12:30 uur.

--- strings/main.py
+++ strings/main.py
@@ -5,52 +5,56 @@ __human_name__ = 'strings'
 # Add your code after this line
 
 #Part 1
-#score1 = 'Ruud Gullit' # = Vraag 1
-#score2 = 'Marco van Basten' # = Vraag 1
+scorer1 = 'Ruud Gullit' # = Vraag 1
+scorer2 = 'Marco van Basten' # = Vraag 1
 
-#goal_0 = 32 # = Vraag 2
-#goal_1 = 54 # = Vraag 2
+goal_0 = 32 # = Vraag 2
+goal_1 = 54 # = Vraag 2
 
 
-#scorers = score1 + ' ' + str(goal_0) + ', ' + score2 + ' ' + str(goal_1) # = Vraag 3
+scorers = scorer1 + ' ' + str(goal_0) + ', ' + scorer2 + ' ' + str(goal_1) # = Vraag 3
 
-#report = (F'{score1} scored in the {goal_0}nd minute.\n{score2} scored in the {goal_1}th minute') # = Vraag 4
-
+report = (F'{scorer1} scored in the {goal_0}nd minute\n{scorer2} scored in the {goal_1}th minute') # = Vraag 4
 
 #Part 2
 player = 'Frank Rijkaard' # = Vraag 1
 
-first_name = (F'{player.find(" ")} {player[0:5]}') # = Vraag 2: slicen + find
-
-last_name_len = (F'{player.find(" ")+1} {player[6:14]} {len(player[6:14])}') # = Vraag 3: find + slicen + len
-
-name_short = 'F. Rijkaard! ' # = Vraag 4
-
-chant = (F'{name_short[3:] * 8}' [:-1]) # = Vraag 5
-print(chant)
-print(chant[1])
-print(chant[-1])
-#print(chant[-1] != ' ')
+first_name = player[0:player.find(" ")] # = Vraag 2: slicen + find
 
 
+last_name_len = len(player[player.find(" "):] [:-1]) # = Vraag 3: len + find + slicen
 
-#good_chant = (!=) Vraag 5 en 6. Deze vragen zijn niet geheel duidelijk.
+name_short = player[0:1] + '.' + ' ' +  player[player.find(" ")+1:] # = Vraag 4
 
+chant = (F'{first_name}! '* 5) [:-1] # = Vraag 5
 
-#print(score1 + ' ' + str(goal_0) + ', ' + score2 + ' ' + str(goal_1)) # Part 1, vraag 3
-#print('\n')
-#print(scorers)
-#print('\n')
-#print(F'{score1} scored in the {goal_0}nd minute')
-#print('\n')
-#print(F'{score2} scored in the {goal_1}th minute')
+#chant = ((first_name + '!' + ' ') * 5) [:-1] # = Vraag 5 --> LET OP!!! Op deze manier is het ook mogelijk.
 
-#print(report)
-#print('\n')
-#print(first_name)
-#print('\n')
-#print(last_name_len)
-#print('\n')
-#print(chant)
-#print('\n')
-#print(2 != 3)
+good_chant = (chant[:-1] != ' ') # = Vraag 6
+
+#Part 1
+print(scorer1) # Part 1: vraag 1
+print('\n')
+print(scorer2) # Part 1: vraag 1
+print('\n')
+print(goal_0) # Part 1: vraag 2
+print('\n')
+print(goal_1) # Part 1: vraag 2
+print('\n')
+print(scorers) # Part 1: vraag 3
+print('\n')
+print(report) # Part 1: vraag 4
+print('\n')
+
+# #Part 2
+print(player) # Part 2: vraag 1
+print('\n')
+print(first_name) # Part 2: vraag 2
+print('\n')
+print(last_name_len) # Part 2: vraag 3
+print('\n')
+print(name_short) # Part 2: vraag 4
+print('\n')
+print(chant) # Part 2: vraag 5
+print('\n')
+print(good_chant) # Part 2: vraag 6
