979e1cc58c514951c8a8f99fef5b6947812a019a
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-04-17 18:28:13 +0200

    Conditions oefening afgerond inc. goedgekeurde wincpy check

--- conditions/main.py
+++ conditions/main.py
@@ -17,41 +17,91 @@ def farm_action(
     action = ''
 
     # Take cows to cowshed
-    if location_of_the_cows == 'pasture' and time_of_day == 'night' or weather == 'rainy':
-         action = 'take cows to cowshed\n'
-         
-    # Milk cows
-    elif cow_milking_status == True and location_of_the_cows == 'cowshed':
-         action = 'milk couws\n'
-         
-             
-    # Fertilize pasture         
-    elif slurry_tank == True and weather != 'sunny' and weather != 'windy' and location_of_the_cows == 'cowshed':
-         action = 'fertilze pasture\n'
-         
-         if location_of_the_cows == 'pasture':
-              action = 'take cows to cowshed\n'
-              action = 'fertilze pasture\n'
-              action = 'take cows back to pasture\n'
-          
-         else:
-              action = 'take cows to cowshed\n'
-    
-    # Mow grass
-    elif grass_status == True and season == 'spring' and weather == 'sunny' and location_of_the_cows != 'pasture':
-         action = 'mow grass\n'
-         action = 'take cows to cowshed\n'
-         action = 'take cows back to pasture\n'
+    if location_of_the_cows == 'pasture' and (time_of_day == 'night' or weather == 'rainy'):
+         action = 'take cow to cowshed'
 
-     # Winpy check
-    if weather == 'sunny' and time_of_day == 'day' and cow_milking_status == True and location_of_the_cows == 'pasture' and season == 'spring' and slurry_tank == False and grass_status == True:
-         action = 'take cows to cowshed\n'
+    # Milk cows      
+    elif location_of_the_cows == 'cowshed' and cow_milking_status == True:
+         action = 'milk cows'
+
+    elif location_of_the_cows == 'pasture' and cow_milking_status == True:
+         action = 'take cows to cowshed\n' + 'milk cows\n' + 'take cows back to pasture'
+
+    # Fertilize pasture
+    elif slurry_tank == True and location_of_the_cows == 'cowshed' and weather != 'sunny' and weather != 'windy':
+         action = 'fertilize pasture'
     
-    # Wait
+    elif slurry_tank == True and location_of_the_cows == 'pasture' and weather != 'sunny' and weather != 'windy':
+         action = 'take cows to cowshed\n' + 'fertilize pasture\n' + 'take cows back to pasture'
+
+    # Mow grass 
+    elif grass_status == True and season == 'spring' and weather == 'sunny' and location_of_the_cows != 'pasture':
+         action = 'mow grass'
+    
+    elif grass_status == True and season == 'spring' and weather == 'sunny' and location_of_the_cows != 'cowshed':
+         action = 'take cows to cowshed\n' + 'mow grass\n' + 'take cows back to pasture'
+    
     else:
-         action = 'wait'
+         action = 'wait'     
     
     return action
+
+print(farm_action('sunny','day', True, 'pasture', 'spring', False, True)) # Wincpy check --> gecheckt.
+print('\n')
+
+einde = 'De output van de \'farm_action\' functie eindigt hierboven.'
+print(einde)
+print('\n')
+
+print(farm_action('rainy', "night", False, 'pasture', 'spring', False, True)) # Take cows to cowshed --> gecheckt.
+print('\n')
+print(farm_action('rainy', "night", True, 'cowshed', 'spring', False, True)) # Milk cows --> gecheckt.
+print('\n')
+print(farm_action('neutral', 'night', False, 'cowshed', 'spring', True, True)) # Fertilize pasture --> gecheckt.
+print('\n')
+print(farm_action('sunny', 'day', False, 'cowshed', 'spring', False, True)) # Mow grass --> gecheckt.
+print('\n')
+print(farm_action('sunny', 'day', False, 'cowshed', 'spring', False, False)) # Wait --> gecheckt.
+print('\n')
+print(farm_action('sunny','day', True, 'pasture', 'spring', False, True)) # Wincpy check --> gecheckt.
+print('\n')
+
+
+
+#     # Take cows to cowshed
+#     if location_of_the_cows == 'pasture' and (time_of_day == 'night' or weather == 'rainy'):
+#          action = 'take cows to cowshed'
+         
+#     # Milk cows
+#     elif cow_milking_status == True and location_of_the_cows == 'cowshed':
+#          action = 'milk couws'
+    
+#     elif location_of_the_cows == 'pasture' and cow_milking_status == True:
+#          action = 'take cows to cowshed\n' + 'milk cows\n' + 'take cows back to pasture'
+
+#     # Fertilize pasture         
+#     elif slurry_tank == True and weather != 'sunny' and weather != 'windy' and location_of_the_cows == 'cowshed':
+#          action = 'fertilze pasture'
+     
+#     elif slurry_tank == True and location_of_the_cows == 'pasture' and weather != 'sunny' and weather != 'windy':
+#          action = 'take cows to cowshed\n' + 'fertilize pasture\n' + 'take cows back to pasture'
+
+#     # Mow grass
+#     elif grass_status == True and season == 'spring' and weather == 'sunny' and location_of_the_cows != 'pasture':
+#          action = 'mow grass'
+     
+#     elif grass_status == True and season == 'spring' and weather == 'sunny' and location_of_the_cows != 'cowshed':
+#          action = 'take cows to cowshed\n' + 'mow grass\n' + 'take cows back to pasture'
+
+     # Winpy check
+#     if weather == 'sunny' and time_of_day == 'day' and cow_milking_status == True and location_of_the_cows == 'pasture' and season == 'spring' and slurry_tank == False and grass_status == True:
+#          action = 'take cows to cowshed'
+    
+    # Wait
+#     else:
+#          action = 'wait'
+    
+#     return action
 
 # print(farm_action('rainy', "night", False, 'pasture', 'spring', False, True)) # Take cows to cowshed --> gecheckt.
 # print('\n')
@@ -63,7 +113,7 @@ def farm_action(
 # print('\n')
 # print(farm_action('sunny', 'day', False, 'cowshed', 'spring', False, False)) # Wait --> gecheckt.
 # print('\n')
-print(farm_action('sunny','day', True, 'pasture', 'spring', False, True)) # Wincpy check --> gecheckt.
+# print(farm_action('sunny','day', True, 'pasture', 'spring', False, True)) # Wincpy check --> gecheckt.
 # print('\n')
 # einde = 'De output van de \'farm_action\' functie eindigt hierboven.'
 # print(einde)
@@ -76,7 +126,4 @@ print(farm_action('sunny','day', True, 'pasture', 'spring', False, True)) # Winc
 
 # print(farm_action('sunny', 'day', True, 'pasture', 'spring', False, True))
 
-"""take cows to cowshed
-milk cows
-take cows back to pasture"""
 
