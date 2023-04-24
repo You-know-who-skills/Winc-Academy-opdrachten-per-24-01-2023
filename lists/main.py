ea677e4911c2306b92cfa7b75aa852d55a698be5
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-04-24 17:06:48 +0200

    Lists oefening per maandag 24-04-2023: ik heb de lijst ingevuld met de film namen

--- lists/main.py
+++ lists/main.py
@@ -6,16 +6,42 @@ __human_name__ = 'lists'
 
 
 # Vraag 1
-def alphabetical_order(film_names) -> str:
+def alphabetical_order(film_names: list) -> str:
 
-    film_names = ['d', 's', 'a', 'e']
+    film_names = ['jaws', 'star wars: episode iv -- a new hope', 'e.t. the extra-terrestrial', 'memoirs of a Geisha']
     
-    film_names.sort()
+    film_names = sorted(film_names)
+
+    print(film_names)
     
-    print(alphabetical_order(film_names))
-    
-    return film_names
+    return True
+
+    # return film_names
+
+    print(film_names)
 
 
+# Vraag 2
+# def  won_golden_globe (film_names) -> bool:
+
+#     film_names = ['d', 's', 'a', 'e']
+    
+#     film_names.sort()
+    
+#     print(alphabetical_order(film_names))
+    
+#     return film_names
+
+
+# Vraag 3
+# def remove_toto_albums(film_names) -> str:
+
+#     film_names = ['d', 's', 'a', 'e']
+    
+#     film_names.sort()
+    
+#     print(alphabetical_order(film_names))
+    
+#     return film_names
 
 
