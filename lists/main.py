3a66759c2e6dcdc3370561ee2da4a6ee7cac8b14
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-04-25 17:27:42 +0200

    Vraag 1, 2 en de 3e bullet van vraag 3 gedaan en getest met Mekso

--- lists/main.py
+++ lists/main.py
@@ -4,43 +4,42 @@ __human_name__ = 'lists'
 
 # Add your code after this line
 
+films = ['Jaws', 'Star Wars: Episode iv -- A New Hope', 'E.T. The Extra-Terrestrial', 'Memoirs of a Geisha', 'Fahrenheit', 'The Seventh One', 'Toto XX', 'Falling in Between', 'Toto XIV', 'Old Is New']
+
+make_films_lower = [films.lower() for films in films]
+
+print(make_films_lower)
+
+del films[:]
+
+print(films)
+
 
 # Vraag 1
-def alphabetical_order(film_names: list) -> str:
+def alphabetical_order(film_names: list) -> list:
 
-    film_names = ['jaws', 'star wars: episode iv -- a new hope', 'e.t. the extra-terrestrial', 'memoirs of a Geisha']
-    
-    film_names = sorted(film_names)
+    film_names.sort()
 
     print(film_names)
     
-    return True
+    return film_names
 
-    # return film_names
 
-    print(film_names)
+# print(alphabetical_order)
 
 
 # Vraag 2
-# def  won_golden_globe (film_names) -> bool:
+# def  won_golden_globe (won_golden_globes_for) -> bool:
 
-#     film_names = ['d', 's', 'a', 'e']
-    
-#     film_names.sort()
-    
-#     print(alphabetical_order(film_names))
+#     won_golden_globes_for = ['jaws', 'star wars: episode iv -- a new hope', 'e.t. the extra-terrestrial', 'memoirs of a Geisha']
     
 #     return film_names
 
 
 # Vraag 3
-# def remove_toto_albums(film_names) -> str:
+# def remove_toto_albums(albums_to_be_removed) -> str:
 
-#     film_names = ['d', 's', 'a', 'e']
-    
-#     film_names.sort()
-    
-#     print(alphabetical_order(film_names))
+#     albums_to_be_removed = ['Fahrenheit', 'The Seventh One', 'Toto XX', 'Falling in Between', 'Toto XIV', 'Old Is New']
     
 #     return film_names
 
