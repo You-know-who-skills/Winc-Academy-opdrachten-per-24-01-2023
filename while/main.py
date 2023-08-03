051fff942e93dcbcb3a627973d11791ee1ad649a
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-08-03 17:05:09 +0200

    Begonnen aan vraag 2 van de 'while' oefening op donderdag 03-08-2023.

--- while/main.py
+++ while/main.py
@@ -8,13 +8,12 @@ __human_name__ = "while"
 
 # Antwoord op vraag 1:
 
+print('\n')
+
+print('Elaboration question 1:\n')
+
 def unique_koala_facts (requested_facts: int) -> list:
-    
-    print('Elaboration question 1:\n')
-    
-    # len_facts = len(facts)
-    # print(len_facts)
-    
+        
     unique_facts = [] # Deze variable betreft een lijst waarin alle unieke feiten komen die je qua aantal wilt zien met de functie 'unique_koala_facts', b.v. 2, 4 of 6 unieke feiten.
         
     count_begin, count_end = 0, 1000    # Met deze code geef je aan hoe vaak de 'while loop' moet gaan loopen door aan te geven bij welk punt / getal de loop moet beginen\
@@ -28,16 +27,42 @@ def unique_koala_facts (requested_facts: int) -> list:
         if fact not in unique_facts:    # Met de code op deze 2 regels zeg je het volgende: 'Als de variabel 'fact' (waarin de functie 'random_koala_fact()' zit (= is het json bestand met alle\
             unique_facts.append(fact)   # willekeurige koala feiten)) niet in de 'unique_facts' lijst zit, voeg deze feit dan toe aan de 'unique_facts' lijst.
                                         # De code op deze 2 regels zorgt er tevens voor dat je geen dubbele feiten krijgt in de 'unique_facts' lijst. Dit doordat je aangeeft dat\
-                                        # alleen als een feit uit het 'random_koala_fact() / jason bestand' nog niet in de 'unique_facts' lijst staat, dan pas het feit mag worden\
+        # count_begin += 1              # alleen als een feit uit het 'random_koala_fact() / jason bestand' nog niet in de 'unique_facts' lijst staat, dan pas het feit mag worden\
                                         # toegevoegd aan de 'unique_facts' lijst.
 
         if count_begin > count_end: # Met de code op deze 2 regels geef je aan dat als de het startpunt van aantal loops (= 'count_begin' = 0) meer is dan\
             break                   # het eindpunt van het aantal loops (= 'count_end' = 1000), pauzeer dan de loop met de 'break' statement.
 
-        count_begin += 1 # Met deze code tel je 1 op bij de variable 'count begin'.
+        count_begin += 1    # Met deze code tel je 1 op bij de variable 'count begin'. Deze code kan je ook tussen de 'if' statements hierboven plaatsen b.v. op regel 30,\
+                            # maar dan buiten de if statements qua tab / indentation.
             
     return unique_facts
 
+
+print('Elaboration question 1:\n')
+
+def num_joey_facts ():
+
+    first_fact = 'joey'
+
+    encountered_facts = 0
+
+    counted_joey_facts = 10
+
+    joey_facts = []
+
+    fact = random_koala_fact()
+
+    while first_fact < counted_joey_facts:
+        if first_fact in fact.lower():
+            
+        if 
+            
+
+
+        encountered_facts += 1
+    
+    return joey_facts
 
 
 
@@ -47,7 +72,8 @@ if __name__ == "__main__":
     
     # facts = (random_koala_fact) # 1e Manier om het 'random_koala_fact' bestand aan te roepen in je functie.
     # unique_koala_facts(random_koala_fact()) # 2e Manier om het 'random_koala_fact' bestand aan te roepen in je functie.
-    print('\n')
     print('Below are the results of the amount of \'koala facts\' which you can choose by entering a number in the \'unique_koala_facts\' function:\n')
     print(unique_koala_facts(2))
-        
+    print('\n')
+    print(num_joey_facts())
+    print('\n')
