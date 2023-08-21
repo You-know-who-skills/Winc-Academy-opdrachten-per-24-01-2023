8fc8a9728fe935df3a48e86c4b64202a1a1e032d
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-08-22 01:11:49 +0200

    De 'while' oefening afgerond en weer verder gegaan met de 'arguments' oefening op maandag 21-08-2023.

--- arguments/main.py
+++ arguments/main.py
@@ -6,13 +6,29 @@ __human_name__ = 'arguments'
 
 print('\n')
 
-def greet(name, greeting = 'Hello, <name>!') -> str:
+def greet(name, greeting = 'Wassup, <name>!') -> str:
     
     return greeting.replace('<name>', name)
 
 
+def force(mass: float, body: str = 'earth') -> int:
+
+    body = {
+        'MERCURY': 0.330,
+        'VENUS' : 4.87,
+        'EARTH' : 5.97,
+        'MOON' : 0.073,
+        'MARS' : 0.642,
+        'JUPITER' : 1898,
+        'SATURN' : 568,
+        'URANUS' : 86.8,
+        'NEPTUNE' : 102,
+        'PLUTO' : 0.0130
+        }
+    
 
 if __name__ == "__main__":
     
     print(greet('Chuck D'))
     print('\n')
+    print(force())
