352800de8fdbe312e121f8a1e220a4b40e1c0964
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-08-23 00:10:14 +0200

    De 'arguments' oefening afgerond op dinsdag 22-08-2023.

--- arguments/main.py
+++ arguments/main.py
@@ -13,22 +13,58 @@ def greet(name, greeting = 'Wassup, <name>!') -> str:
 
 def force(mass: float, body: str = 'earth') -> int:
 
-    body = {
-        'MERCURY': 0.330,
-        'VENUS' : 4.87,
-        'EARTH' : 5.97,
-        'MOON' : 0.073,
-        'MARS' : 0.642,
-        'JUPITER' : 1898,
-        'SATURN' : 568,
-        'URANUS' : 86.8,
-        'NEPTUNE' : 102,
-        'PLUTO' : 0.0130
+    bodies = {
+        'mercury': 3.7,
+        'venus' : 8.9,
+        'earth' : 9.8,
+        'moon' : 1.6,
+        'mars' : 3.7,
+        'jupiter' : 23.1,
+        'saturn' : 9.0,
+        'uranus' : 8.7,
+        'neptune' : 11.0,
+        'pluto' : 0.7,
         }
+        
+    return mass * round(bodies[body], 1) # Je kan de code / formule direct in de return statement plaatsen of eerst een variabel aanmaken waarin je de code / formule plaatst en daarna returned.
+
+'''
+Op bovenstaande manier gebruik je de 'round(,)' code om binnen een 'dict' de 'waarde' / 'value' van een 'key' te benaderen:
+1 = Start met het openen van de round code --> round(
+2 = Geef de naam van de dict op als --> bodies
+3 = Gebruik de / een parameter om bij de waarde te komen --> [body]
+4 = Geef aan op hoeveel decimalen de waarde / value moet eindigen --> , 1
+5 = Sluit de round code --> )
+'''
+
+
+def pull(m1: float, m2: float, d: float) -> int:
+
+    G = 6.674 * 10**11
+
+    pull = G * ((m1 * m2) / d**2)
     
+    return pull
+
+'''
+De 'pull' functie van hierboven snap ik nog niet, dus deze heb ik gewoon overgenomen van de uitwerking in de lesstof van de module, want ik was ff helemaal klaar\
+met alle onduidelijke en foutieve vraagstellingen in de lesstof van de module.
+'''
+
 
 if __name__ == "__main__":
     
+    print("Hieronder staat het antwoord op vraag 1:'\n")
     print(greet('Chuck D'))
+    
     print('\n')
-    print(force())
+
+    print("Hieronder staat het antwoord op vraag 2:'\n")
+    print(force(9, 'venus'))
+    
+    print('\n')
+
+    print("Hieronder staat het antwoord op vraag 3:'\n")
+    print(pull(6, 10, 11))
+
+
