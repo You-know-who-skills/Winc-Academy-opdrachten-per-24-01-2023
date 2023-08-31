9d418910f2a9cf23444068b5a2a52406370077cb
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-08-31 23:59:23 +0200

    Verder gegaan met vraag 1 van de 'Soccer Player Comparison' oefening op donderdag 31-08-2023.

--- classes/main.py
+++ classes/main.py
@@ -4,6 +4,8 @@ __human_name__ = 'classes'
 
 # Add your code after this line
 
+print('\n')
+
 class Player():
     def __init__(self, name: str, speed: float, endurance: float, accuracy: float) -> str:
 
@@ -16,8 +18,31 @@ class Player():
         print(F"Hello everyone, my name is {self.name}.")
 
     def strength(self):
+        
+        best = (None, 1)
         for attr in ["speed", "endurance", "accuracy"]:
+
+            value = getattr(self, attr)
             
+            if value <= best[-1]:
+                best = (attr, value)
+            
+            else:
+                raise ValueError(F"Please make sure that {attr} is between 0 and 1.\n")
+        
+        return best
+
+if __name__ == "__main__":
+
+    rijkaard = Player("Rijkaard", 0.1, 0.2, 0.3)
+    gullit = Player("Gullit", 0.4, 0.5, 0.6)
+    seedorf = Player("Seedorf", 0.7, 0.8, 0.9)
+
+    print(rijkaard.strength())
+    print(gullit.strength())
+    print(seedorf.strength())
+    
+    
 
 
 
