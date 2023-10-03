b8bca30ff25877380fc6154c03c2c60fd339a774
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-10-03 17:13:43 +0200

    Begonnen aan de 'modules' oefening van de module 'Modules, Libraries, Third Party Packages' op dinsdag 03-10-2023.

---
+++ modules/main.py
@@ -0,0 +1,25 @@
+# Do not modify these lines
+__winc_id__ = '78029e0e504a49e5b16482a7a23af58c'
+__human_name__ = 'modules'
+
+# Add your code after this line
+
+# Modules
+
+import this
+import math
+import sys
+import datetime as date_t
+
+
+def wait(seconds: int):
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+
