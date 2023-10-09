b3459464891208390480c6fd56c5845c97c5cbf1
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-10-09 22:49:42 +0200

    Verder gegaan met de oefening 'Modules' en ik ben nog bezig met de datetime module en tegelijkertijd bezig met de vraag over het importeren van het zelf gemaakte 'greet.py' bestand.

---
+++ modules/greet.py
@@ -0,0 +1,17 @@
+
+'''
+Create a new file called 'greet.py', and in that file write a function called 'super_greeting' that takes a 'name' as an argument (str) and returns the following string: 'Hellooo...ooo, Bob!'.
+'''
+
+def super_greeting(name: str) -> str:
+
+    return F"Hellooo...ooo, {name}!"
+
+
+if __name__ == "__main__":
+
+    print('\n')
+
+    print(super_greeting('Chuck D'))
+
+    print('\n')
--- modules/main.py
+++ modules/main.py
@@ -6,45 +6,104 @@ __human_name__ = 'modules'
 
 # Modules
 
-# import this
+'''
+Question 1 = At the top of this file, import a module that prints the 'Zen of Python'.
+'''
+
+print('\n')
+
+import this
+
+print('\n')
+
+'''
+Import the following modules: time, math and sys.
+'''
 
 import time
 import math
 import sys
+
+import greet as sp
+
+
+
+'''
+Also import the 'datetime object' from the 'datetime module'. Use an alias here (like 'dt' for example).
+'''
+
 import datetime as dt
-# from datetime import datetime
 
 
+'''
+Write a function called 'wait' that takes one argument called 'seconds' (int), that uses a function in the time module to make the computer wait for that number of seconds and then returns nothing.
+'''
 
 def wait(seconds: int):
 
-    from time import sleep # De sleep code telt het aantal ingevoerd seconden af en returnd dan wat 
+    # return "Hallo" # Deze return statement doet het dus ook.
 
-    time.sleep(seconds) # Deze code met onderstaande "Hallo" return statement doen het ook.
+    return time.sleep(seconds) # Deze code doet het ook. De sleep code telt het aantal ingevoerde seconden af en returnd daarna alles wat je in de return statement zet.
 
-    return "Hallo" 
 
-    # return time.sleep("seconds") # Deze code doet het.
-
+'''
+Write a function called 'my_sin' that takes one float as an argument and returns the 'sine' of that float. Use the math module.
+'''
 
 def my_sin(float: float):
 
-    from math import sin
-
     return math.sin(float)
 
+
+'''
+Write a function called 'iso_now' that takes no arguments and returns a string containing the current date and time up to the minute in the ISO 8601 format. Use the datetime module.
+
+Example: 2000-12-31T17:00.
+'''
 
 def iso_now():
 
-    from dt import sin
+    # return dt.datetime.now().isoformat()
+    # return dt.datetime.isoformat(now)
+    # return dt.datetime.isoformat()
+    return dt.datetime.now()
 
-    return math.sin(float)
 
+'''
+Write a function called 'platform' that takes no arguments and returns a string that shows which operating system (Linux, Windows, macOS, and so on) we are on. Use the sys module.
+'''
+
+def platform():
+
+    return sys.platform
+
+
+
+def supergreeting_wrapper(name):
+
+    return from sp import greet.name
 
 
 
 
 if __name__ == "__main__":
 
-    print(wait(3)) # Deze code doet het.
+    print(wait(1)) # Deze code doet het.
+    
+    print('\n')
+    
     print(my_sin(1.2))
+
+    print('\n')
+
+    print(iso_now())
+
+    print('\n')
+
+    print(platform())
+
+    print('\n')
+
+    print(supergreeting_wrapper)
+
+    print('\n')
