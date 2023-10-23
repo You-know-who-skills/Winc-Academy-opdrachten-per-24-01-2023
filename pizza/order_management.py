6d020cf565747d80ce6babff734b54be59f0a969
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-10-23 20:55:58 +0200

    Verder gegaan met mijn aantekeningen bij de 'pizza' oefening. Ik ben gebleven bij de 'save_orders_to_list' functie op regel 98 op maandag 23-10-2023.

--- pizza/order_management.py
+++ pizza/order_management.py
@@ -56,19 +56,17 @@ Return an 'instance' of PizzaOrder, with the 3 arguments / variables as attribut
 '''
 
 
-def create_order(): # Dit stond niet in de oefening, maar deze functie moet buiten de class worden geplaatst. Dus na een gesprek met een Winc mentor op dinsdag 17-10-2023 tussen 11:00 en 11:45\
-                    # uur ben ik hierachter gekomen.
+def create_order(): # Dit stond niet in de oefening, maar deze functie moet buiten de class worden geplaatst. Dus na een gesprek met een Winc mentor op dinsdag 17-10-2023 tussen 11:00 en 11:45 uur ben ik hierachter gekomen.
 
-    customer_name = input("Please enter your name: ")   # Met de 'input()' code zorg je ervoor dat de gebruiker iets in kan vullen. LET OP!!! De input code werkt ook zonder dat je de variabel\
-    # print(customer_name)                              # definieert.
+    customer_name = input("Please enter your name: ")   # Met de 'input()' code zorg je ervoor dat de gebruiker iets in kan vullen. LET OP!!! De input code werkt ook zonder dat je de variabel print(customer_name) definieert.
     
     pizza_type = input("Please choose your pizza type: ")
     # print(pizza_type)
 
-    toppings = input("Please choose your toppings and please separate each topping with a comma: ").split(",")  # Door de 'split()' code hier te gebruiken met de komma als separator, zorg je\
-    # print(toppings)                                                                                           # ervoor dat de items in deze input worden gescheiden door de komma.
+    toppings = input("Please choose your toppings and please separate each topping with a comma: ").split(",")  # Door de 'split()' code hier te gebruiken met de komma als separator, zorg je ervoor dat de items in deze input worden\
+    # print(toppings)                                                                                           # gescheiden door de komma.
 
-    return PizzaOrder(customer_name, pizza_type, toppings)
+    return PizzaOrder(customer_name, pizza_type, toppings) # Met deze 'return' statement zorg je ervoor dat de gebruiker iets kan invullen bij de 3 variabelen die hierboven staan.
 
 
 '''Question 3 = Save the order to a file.
@@ -77,25 +75,27 @@ The next function will save the order to a file. In this function we are going t
 
 You will need to access a 'txt' file and write in it with Python. We will use the 'open()' function and the '.write()' method.
 
-Define a function called 'save_orders_to_file' in the 'order_management' module, which takes a list of 'PizzaOrder' objects (order_list, for example) and saves them to a file named "orders.txt"\
-in the current directory. Each PizzaOrder object should be written to a new line in the file.
+Define a function called 'save_orders_to_file' in the 'order_management' module, which takes a list of 'PizzaOrder' objects (order_list, for example) and saves them to a file named "orders.txt" in the current directory. Each PizzaOrder\
+object should be written to a new line in the file.
 
-Create a 'orders.txt' file in your project folder. Use the with statement to open the file: 'with open('./orders.txt', 'w') as file:' With this we are opening the file by giving the path and\
-writing mode as arguments.
+Create a 'orders.txt' file in your project folder. Use the with statement to open the file: 'with open('./orders.txt', 'w') as file:' With this we are opening the file by giving the path and writing mode as arguments.
 
 Within the 'with' statement, add a 'for loop' to iterate among pizzas in the 'order_list' and make it write an 'f-string' that uses the attributes of the pizza object:
-'(file.write())'
+
+- '(file.write())'
 
 It's vital to use the newline character '(/n)' at the end of the string, so every pizza is written in a new line of the '.txt file'.
 '''
 
 def save_orders_to_file(order_list):
 
-    with open('./orders.txt', 'w') as file:
+    with open('./orders.txt', 'w') as file: # De 'with' statement / code kan je gebruiken om een blok met / regel van met code uit te (laten) voeren. Met de 'open()' code kan je bestanden openen. Met de letter 'w' (dat voor 'write' staat)\
+                                            # in deze code geef je aan dat je in het geopende bestand wil gaan schrijven.
 
-        for order in order_list:
-            # file.write(F"Customer name = {order.customer_name}\nPizza type = {order.pizza_type}\nToppings = {order.toppings}\n")
-            file.write(F"{order.customer_name}, {order.pizza_type}, {order.toppings}\n")
+        for order in order_list: # Met de variabel 'order' ga ik itereren over alles wat in de variabel 'order_list' staat. En hieronder ga ik bepalen wat er in 'order_list' komt te staan.
+            
+            file.write(F"{order.customer_name}, {order.pizza_type}, {order.toppings}\n")    # Met deze code geef je aan wat er in de 'order_list' variabel moet komen te staan om erover te kunnen itereren. En dit heb ik gedaan middels een\
+                                                                                            # 'F-string'.
 
 
 '''
@@ -137,7 +137,8 @@ def load_orders_from_file():
 
     orders = []
 
-    with open('./orders.txt', 'r') as file:
+    with open('./orders.txt', 'r') as file: # De 'with' statement / code kan je gebruiken om een blok met / regel van met code uit te (laten). Met de 'open()' code kan je bestanden openen. Met de letter 'r' (dat voor 'read' staat) in deze\
+                                            # code geef je aan dat je het geopende bestand wil lezen.
     
         for order in file:
             order_data = order.strip().split(', ')
@@ -147,7 +148,7 @@ def load_orders_from_file():
                 customer_name, pizza_type, toppings, order_time, is_delivered = order_data
 
                 toppings = toppings.split('|')
-                order_time = dt.strptime(order_time, "%Y-%m-%d %H:%M:%S.%f")
+                # order_time = dt.strptime(order_time, "%Y-%m-%d %H:%M:%S.%f")
                 is_delivered = is_delivered == 'True'
                 order = PizzaOrder(customer_name, pizza_type, toppings, order_time, is_delivered)
                 orders.append(order)
