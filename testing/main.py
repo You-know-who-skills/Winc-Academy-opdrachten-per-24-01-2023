32bbcab67534f779a804f6c5cd95593a1c376daf
Anonymous11-2022 <117989739+Anonymous11-2022@users.noreply.github.com>
2023-11-17 17:15:53 +0100

    Verder gegaan met de 'testing' oefening en deze ook afgerond op vrijdag 17-11-2023.

--- testing/main.py
+++ testing/main.py
@@ -1,10 +1,107 @@
 
+'''
+In main.py, write the function get_none that should take no arguments and always return None.
+'''
 
 def get_none():
 
     # return "This is not 'None'."
     return None
 
+    # try:
+    #     if get_none() == None:
+    #         return True
+
+    # except TypeError:             # Deze foutmelding komt niet tevoorschijn wanneer ik iets anders dan 'None' invul bij de 'return' statement in het 'main.py' bestand. LET OP!!! DIT FF NAVRAGEN BIJ EEN WINC MENTOR.
+    #     return "Ceck again"
+
+'''
+In main.py, implement the function flatten_dict so that it passes the tests you wrote in test_flatten_dict.
+
+Another example:
+
+flatten_dict({'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14})
+# [{'inner_a': 42, 'inner_b': 350}, 3.14]
+
+
+'''
+
+
+def flatten_dict(dictionary) -> list:
+
+    # dictionary = {
+    #     'a': a,
+    #     'b': b
+    # }
+    
+    # flatten = dictionary.values()
+
+    flatten = []
+
+    for value in dictionary.values():
+
+        flatten.append(value)
+        
+    # print(dictionary)
+
+    # nieuw_flatten = [dictionary]
+
+    return flatten
+    
+    # print(waarde)
+    # print(waarde.keys())
+    # print(waarde.values())
+    # nieuwe_waarde = list(waarde.values())
+
+
+    # print(waarde)
+    # print(waarde.keys())
+    # print(waarde.values())
+    # nieuwe_waarde = list(waarde.values())
+    
+    
+    # print(flatten_dict(nieuwe_waarde))
+
+
+
+    # return nieuwe_waarde
+
+'''
+Run the tests you wrote with pytest as you are working on the implementation of the flatten_dict function. 
+
+For an extra challenge, see if you can implement flatten_dict in such a way that the dictionary is flattened all the way down regardless of how many nested levels the original dictionary contains. 
+
+For example:
+
+flatten_dict({'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14})
+
+# [42, 350, 3.14]
+
+flatten_dict({'a': [{'inner_inner_a': 42}]})
+
+# [42]
+
+
+Note:
+To be able to do this, you may want to look into a technique called recursion.
+'''
+
+
+# def add_numbers(a, b): # Dit is een voorbeeld functie die ik van mentor Yordy heb gekregen en is dus geen vraag van de 'testing' oefening.
+
+#     return a * b
+
+    # try:  
+    #     return a / b  
+    # except ZeroDivisionError:  
+    #     raise ZeroDivisionError("Division by zero is not allowed")
+
 if __name__ == "__main__":
 
-    get_none()
+    print(get_none())
+    # print(add_numbers(5, 10))
+    # print(flatten_dict({'a': {'inner_a': 42, 'inner_b': 350}, 'b': 3.14}))  # Deze code geeft een foutmelding waneer je de pytest runt, maar dit is wel de 'For an extra challenge'. Om deze code uit te kunnen voeren moet je waarschijnlijk\
+                                                                            # eerst nog een functie schrijven om de lijst uit de dictionary te halen, of iets dergelijks. De mentor had geen tijd meer om dit goed uit te leggen.
+    # print(flatten_dict({'a': 6}))
+    # print(flatten_dict({42, 3.14}))
+    print(flatten_dict({42, 3.14}))
--- testing/test_main.py
+++ testing/test_main.py
@@ -1,34 +1,45 @@
 
-from main import get_none
+import main
+
+'''
+In test_main.py, write a function test_get_none that tests if a function from main.py called get_none returns None.
+'''
 
 
 def test_get_none():
-
-    # try:
-    #     if get_none() == None:
-    #         return True
-
-    # except TypeError:             # Deze foutmelding komt niet tevoorschijn wanneer ik iets anders dan 'None' invul bij de 'return' statement in het 'main.py' bestand. LET OP!!! DIT FF NAVRAGEN BIJ EEN WINC MENTOR.
-    #     return "Ceck again"
     
-    try:
-        if get_none() == None:
-            return True
+    assert main.get_none() == None
 
-    except TypeError:
-        return "Ceck again"
 
+'''
+In test_main.py, write a function test_flatten_dict that tests if a function flatten_dict, when given a dictionary (dict) as its argument, returns the values of that dictionary in a list (list).
+
+For example: 
+
+flatten_dict({'a': 42, 'b': 3.14})
+# [42, 3.14]
+flatten_dict({'a': [42, 350], 'b': 3.14})
+# [[42, 350], 3.14]
+
+Tip: Try to think about the tests you're writing as a tiered system (= gelaagd systeem).
+
+First, you might test if the value returned by flatten_dict is a list at all. Then you could test the function with a very small (single element) dictionary, then more complex dictionaries. 
+
+This way if a test fails, you know not only that it fails, but also exactly where it fails. The more detailed the test levels, the better you can track the failure if the test fails.
+
+'''
 
 def test_flatten_dict():
     
-    try:
-        if flatten_dict == ({'a': 42, 'b': 3.14}):
-        # [42, 3.14]
+    assert main.flatten_dict({'a': 42, 'b': 3.14}) == [42, 3.14]
+            # [42, 3.14]
     
-        flatten_dict({'a': [42, 350], 'b': 3.14})
-        # [[42, 350], 3.14]
+    # assert flatten_dict({'a': [42, 350], 'b': 3.14})
+            # [[42, 350], 3.14]
 
 
 if __name__ == "__main__":
 
     print(test_get_none())
+    print(test_flatten_dict())
+    # print(test_add_numbers())
